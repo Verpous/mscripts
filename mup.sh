c4a4e45241a6b2a263b4df691247dfa0cab31a5c
Verpous <ederyaviv@gmail.com>
2022-08-23 19:39:27 +0300

    added GPL notices

--- mup.sh
+++ mup.sh
@@ -1,4 +1,19 @@
 #! /bin/bash
+
+# Copyright (C) 2022 Aviv Edery.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 ## Fetches imdb lists and updates the local json store for them and the resulting category files.
 ## LIST is either a list name as defined in the configuration file, or an IMDb list ID (everything is case-insensitive).
 ## Can pull several lists at once. If no LIST provided, downloads lists marked as default in the configuration.
